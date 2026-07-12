5bc2a899af4cac2b289a67e3657861b1dea2e685
SongMyungsik <88374975+SongMyungsik@users.noreply.github.com>
2026-07-12 21:46:01 +0900

    Deploy Nahum word annotations (558 words, complete book)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1034249057" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2300727726" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
