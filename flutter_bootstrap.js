07fd0622bd7065ffbfff9b983141a18d09a00956
SongMyungSik <dream70@naver.com>
2026-08-20 08:42:26 +0900

    원어 성경 화면 레이아웃 조정 반영 웹 앱 재배포

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "535737901" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1417184485" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
